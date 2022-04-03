d1a0cbb95fe139e512b83b2efb5a3b3edf784aa8
Void Bringer <100240410+voidbringer666@users.noreply.github.com>
2022-04-03 22:03:15 +0300

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -39,7 +39,9 @@ class App{
 	}	
     
     resize(){
-        
+        this.camera.aspect = window.innerWidth/window.innerHeight;
+	this.camera.updateProjectionMatrix();
+	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
     }
     
 	render( ) {  
